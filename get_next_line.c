a84a08484584603f8ded59f4fc8e7a00b4165948
OnkelPony <zkurvenej@outlook.com>
2023-02-11 17:20:17 +0100

    Created necessary files.

---
+++ get_next_line.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jimartin <jimartin@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/11 16:33:42 by jimartin          #+#    #+#             */
+/*   Updated: 2023/02/11 16:40:34 by jimartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include get_next_line.h
+
+/*
+Write a function that returns a line read from a file descriptor
+fd: The file descriptor to read from
+Return: Read line - correct behavior
+NULL - there is nothing else to read, or an error occurred
+*/
+char	*get_next_line(int fd)
+{
+}
