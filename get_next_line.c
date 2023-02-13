19bb11bf371567bc7299aad90490010e92f65fc8
Jiri Martinek <jimartin@c2r3s2.42prague.com>
2023-02-13 19:11:31 +0100

    Fixed imports.

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,11 @@
 /*   By: jimartin <jimartin@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 16:33:42 by jimartin          #+#    #+#             */
-/*   Updated: 2023/02/11 16:40:34 by jimartin         ###   ########.fr       */
+/*   Updated: 2023/02/13 19:10:08 by jimartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include get_next_line.h
+#include "get_next_line.h"
 
 /*
 Write a function that returns a line read from a file descriptor
@@ -20,4 +20,13 @@ NULL - there is nothing else to read, or an error occurred
 */
 char	*get_next_line(int fd)
 {
+	char *line;
+	int n;
+
+	line = malloc(sizeof(*line + 1));
+	if (!line)
+		return (NULL);
+	while ((n = read(fd, line, 1)) > 0)
+		;
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,15 @@
 /*   By: jimartin <jimartin@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 16:34:39 by jimartin          #+#    #+#             */
-/*   Updated: 2023/02/11 16:37:53 by jimartin         ###   ########.fr       */
+/*   Updated: 2023/02/13 19:10:15 by jimartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
 
